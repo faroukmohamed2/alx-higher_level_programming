5c2e8961669487e0ac213407d49003be8dd6581d
farouk <farmo6995@gmail.com>
2024-01-24 23:28:15 +0200

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -12,5 +12,6 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i]))
+    if count > 0: 
+        for i in range(count):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
