bc07cfa2f3d64b0dc04beea9666ff8e51727d2d4
farouk <farmo6995@gmail.com>
2024-03-14 16:48:53 -0700

    pyton modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,7 @@ if __name__ ==" __main__":
     '''
     print a + b
     '''
-    from add_0.py import add
+    from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b))
+    print("{} + {} = {}".format(a, b, add(a, b)))
