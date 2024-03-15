74dbb8b4b85d31e62f714ec96f4edb75b76ec367
farouk <farmo6995@gmail.com>
2024-03-15 03:04:45 +0200

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+export PYTHONPATH="D:\alx\alx-higher_level_programming\0x02-python-import_modules\hidden_4.pyc":$PYTHONPATH
 if __name__ == "__main__":
     """Print hidden_4 module"""
     import hidden_4
