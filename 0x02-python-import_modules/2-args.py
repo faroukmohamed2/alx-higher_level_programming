bc101c6b077dac2c7f74fd9a5de0a86032ab1136
farouk <farmo6995@gmail.com>
2024-03-14 17:14:44 -0700

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """argv"""
+    import sys
+
+    size = len(sys.argv) - 1
+    if size  == 0:
+        print("0 arguments.")
+    elif size == 1:
+        print("1 argument:")
+        print("1: " + sys.argv[0])
+    else:
+        print("{} arguments".format(size))
+        for i in range(size):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
