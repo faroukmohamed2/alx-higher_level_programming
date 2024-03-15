7f31d23c0ba772e41f8d4230eec99d862ba6ec58
farouk <farmo6995@gmail.com>
2024-03-15 02:56:00 +0200

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     """hidden"""
     import hidden_4.pyc
-    names = dir(hidden_4.pyc)
+    names = dir(hidden_4)
     for name in names:
         if name[:2} != "__":
             print(name)
